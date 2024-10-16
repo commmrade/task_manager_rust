99397d18134310ed6eeee2051a47b7716a7d413e
klewy <cigankov807@gmail.com>
2024-10-16 14:20:14 +0300

    update

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
-use axum::{extract::{rejection::QueryRejection, Query}, http::{Result, StatusCode}, Json};
+use std::sync::Arc;
+
+use axum::{extract::{rejection::QueryRejection, Query}, http::StatusCode, routing::{get, post}, Json, Router};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
@@ -8,16 +10,44 @@ struct QUser {
 }
 
 
+struct AppState {
+    
+}
+impl Default for AppState {
+    fn default() -> Self {
+        return Self{}
+    }
+}
+
 #[tokio::main]
 async fn main() {
     println!("hello world");
-    let app = 
 
+    let app_state = Arc::new(AppState::default());
+    let app = Router::new()
+    .route("/welcome", get(welcome))
+    .route("/login", post(login))
+    .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn welcome(result : Result<Query<QUser>, QueryRejection>) -> Result<Json<QUser>, (StatusCode, String)> {
 
-    
-
-    Ok(Json(QUser { name: "fuck".to_string(), age: 18 }))
+async fn welcome(result : Result<Query<QUser>, QueryRejection>) -> Result<(), (StatusCode, String)> {
+    match result {
+        Ok(Query(result)) => {
+            todo!();
+        }
+        Err(_) => {
+            return Err((StatusCode::BAD_GATEWAY, "Error".to_string()))
+        }
+    }
 }
+
+async fn login(result : Result<Query<QUser>, QueryRejection>) -> Result<(), (StatusCode, String)> {
+
+    todo!("Implemnt test login klewy 1488");
+}
+
