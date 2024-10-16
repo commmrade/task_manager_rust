5db3cfc38be04a69abc1815191d696998bb9c450
klewy <cigankov807@gmail.com>
2024-10-16 13:49:59 +0300

    upd

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::{rejection::QueryRejection, Query}, http::{Result, StatusCode}, Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct QUser {
+    name: String,
+    age: u32
+}
+
+
+#[tokio::main]
+async fn main() {
+    println!("hello world");
+    let app = 
+
+}
+
+async fn welcome(result : Result<Query<QUser>, QueryRejection>) -> Result<Json<QUser>, (StatusCode, String)> {
+
+    
+
+    Ok(Json(QUser { name: "fuck".to_string(), age: 18 }))
 }
